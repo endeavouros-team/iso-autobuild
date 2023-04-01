a66a9780c977180c0ea943b3edda532264c0170b
Johannes Kamprad <archlinux@kamprad.net>
2023-04-01 14:23:02 +0200

    [prepare-action.sh] comment run_before_squashfs.sh patching (not needed anymore)

--- prepare-action.sh
+++ prepare-action.sh
@@ -9,7 +9,7 @@ patch EndeavourOS-ISO/packages.x86_64 < packages.x86_64.patch
 patch EndeavourOS-ISO/profiledef.sh < profiledef.sh.patch
 
 # patch run_before_squashfs.sh to remove github folder before squashfs:
-patch EndeavourOS-ISO/run_before_squashfs.sh < run_before_squashfs.sh.patch
+# patch EndeavourOS-ISO/run_before_squashfs.sh < run_before_squashfs.sh.patch
 
 # copy live session Wallpaper into ISO structure:
 cp livewall.png EndeavourOS-ISO/airootfs/root/
