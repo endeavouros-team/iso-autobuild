c982c0b5e91c40e54fa40d3edf6a64af86a70211
Johannes Kamprad <archlinux@kamprad.net>
2023-12-11 14:44:50 +0100

    Update build-packages.sh

--- build-packages.sh
+++ build-packages.sh
@@ -2,12 +2,12 @@
 # building calamares git packages:
 echo "%wheel         ALL = (ALL) NOPASSWD: ALL" >> /etc/sudoers
 pacman -Syy
-#pacman -S --noconfirm --needed cmake extra-cmake-modules kpmcore boost python-jsonschema python-pyaml python-unidecode qt5-svg qt5-webengine yaml-cpp networkmanager upower kconfig kservice squashfs-tools rsync qt5-xmlpatterns doxygen dmidecode gptfdisk hwinfo kparts solid qt5-tools libpwquality ckbcomp qt5-quickcontrols2
+pacman -S --noconfirm --needed cmake extra-cmake-modules kpmcore boost python-jsonschema python-pyaml python-unidecode qt5-svg qt5-webengine yaml-cpp networkmanager upower kconfig kservice squashfs-tools rsync qt5-xmlpatterns doxygen dmidecode gptfdisk hwinfo kparts solid qt5-tools libpwquality ckbcomp qt5-quickcontrols2
 useradd -m -G wheel -s /bin/bash build
 cd /home/build
 mkdir -p /home/build/packages
 chown -R build:build /home/build/packages
 sudo -u  build git clone https://github.com/endeavouros-team/PKGBUILDS
 cd /home/build/PKGBUILDS/calamares-git
-sudo -u  build makepkg -f -s 
+sudo -u  build makepkg -f
 cp *.pkg.tar.zst /home/build/packages/
