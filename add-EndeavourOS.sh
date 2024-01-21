1fb257acb34679011add6b3ff7e826301258f310
Johannes Kamprad <archlinux@kamprad.net>
2024-01-21 22:47:08 +0100

    [add-EndeavourOS.sh] removed overdone --init

--- add-EndeavourOS.sh
+++ add-EndeavourOS.sh
@@ -6,7 +6,7 @@ pacman-key --init && pacman -Syu --noconfirm && pacman -Syu base-devel --noconfi
 pacman -Sy --noconfirm --needed archlinux-keyring
 cp  "pacman.conf" "/etc/pacman.conf"
 cp  "endeavouros-mirrorlist" "/etc/pacman.d/endeavouros-mirrorlist"
-pacman-key --init && pacman-key --recv-key 0F20FADC599D1C46EB556455AED8858E4B9813F1 --keyserver keyserver.ubuntu.com && pacman-key --lsign-key 0F20FADC599D1C46EB556455AED8858E4B9813F1
+pacman-key --recv-key 0F20FADC599D1C46EB556455AED8858E4B9813F1 --keyserver keyserver.ubuntu.com && pacman-key --lsign-key 0F20FADC599D1C46EB556455AED8858E4B9813F1
 pacman-key --init
 pacman -Sy --noconfirm endeavouros-keyring 
 pacman -S --noconfirm endeavouros-mirrorlist
