a2938b889a48140aa3c5437b5f34138e418462f6
Johannes Kamprad <archlinux@kamprad.net>
2024-01-30 22:40:07 +0100

    [build-packages.sh] plasma5support plasma-framework5 (calamares build fails)

--- build-packages.sh
+++ build-packages.sh
@@ -2,7 +2,7 @@
 # building calamares git packages:
 echo "%wheel         ALL = (ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 pacman -Syy
-pacman -S --noconfirm --needed git cmake extra-cmake-modules kpmcore boost python-jsonschema python-pyaml python-unidecode gawk qt5-svg qt5-webengine yaml-cpp networkmanager upower kcoreaddons5 kconfig5 ki18n5 kservice5 kwidgetsaddons5 kpmcore squashfs-tools rsync cryptsetup qt5-xmlpatterns doxygen dmidecode gptfdisk hwinfo kparts5 polkit-qt5 python solid5 qt5-tools boost-libs libpwquality ckbcomp qt5-quickcontrols2
+pacman -S --noconfirm --needed plasma-framework5 plasma5support git cmake extra-cmake-modules kpmcore boost python-jsonschema python-pyaml python-unidecode gawk qt5-svg qt5-webengine yaml-cpp networkmanager upower kcoreaddons5 kconfig5 ki18n5 kservice5 kwidgetsaddons5 kpmcore squashfs-tools rsync cryptsetup qt5-xmlpatterns doxygen dmidecode gptfdisk hwinfo kparts5 polkit-qt5 python solid5 qt5-tools boost-libs libpwquality ckbcomp qt5-quickcontrols2
 useradd -m -G wheel -s /bin/bash build
 cd /home/build
 mkdir -p /home/build/packages
