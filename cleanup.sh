580d616948d64b7ac9743febc3581b04a02c108e
Johannes Kamprad <archlinux@kamprad.net>
2025-10-25 20:15:12 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-rm -rf "work"
-rm -rf airootfs/etc/pacman.d/
+echo "Cleaning up ISO build structure..."
+rm -rf "EndeavourOS-ISO/work"
+rm -rf EndeavourOS-ISO/airootfs/etc/pacman.d/
+echo "Cleaning up large temporary directories..."
+rm -rf node_modules dist build logs tmp .cache
